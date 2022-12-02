d5334e93c73aedcb815195ca8a946cf0419789c3
Zen <muhzaini30@gmail.com>
2022-12-02 22:41:01 +0800

    Oke

---
+++ script/sitemap.mjs
@@ -0,0 +1,25 @@
+import { SitemapManager } from "sitemap-manager";
+import cup from "cup-readdir";
+
+const MySitemap = new SitemapManager({
+  /* Options here */
+  siteURL: "https://rumahzen.thedev.id/" /*URL*/,
+  pathPrefix: "" /*Site Prefix*/,
+  outDir: "" /*Output dir*/,
+});
+
+let data = await cup.getAllFilePaths("output");
+data = data.filter((x) => x.endsWith(".html"));
+for (let x of data) {
+  MySitemap.addUrl(/*Category name here*/ "rumahzen", [
+    {
+      loc: `https://rumahzen.thedev.id/${x.replace("output/", "")}` /*URL*/,
+      lastmod: new Date() /*Last modification time*/,
+      changefreq: 7 /*Change frequence*/,
+      priority: 1.0 /*Priority*/,
+    },
+    /*You can add more!*/
+  ]);
+}
+
+MySitemap.finish();
